4f02da06481df1282049e15e6c7e8397f70f21fe
Prantho-das <pranthokumardas786@gmail.com>
2026-06-12 23:43:50 +0600

    chore: added AdSense ads.txt file and verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
         <link rel="icon" href="/logo.png" />
         <link rel="apple-touch-icon" href="/logo.png" />
         {/* Google AdSense Script */}
+        <meta name="google-adsense-account" content="ca-pub-8440441791918870" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8440441791918870" crossOrigin="anonymous"></script>
         {/* Theme Initialization Script */}
         <script dangerouslySetInnerHTML={{
